f362bc5f7642d32a1b68de2f156cc5f5c4eac4f0
Giovanni Liboni <giovanni@liboni.net>
2020-10-22 20:52:20 +0200

    chore(project): 🔧 Add swagger

--- src/module/config/app.nestjs.config.ts
+++ src/module/config/app.nestjs.config.ts
@@ -1,6 +1,15 @@
 import { INestApplication } from '@nestjs/common';
 import ExpressRequestId from 'express-request-id';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export default (app: INestApplication) => {
   app.use(ExpressRequestId());
+
+  const options = new DocumentBuilder()
+    .setTitle('Nestjs')
+    .setDescription('The Nestjs API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 };
